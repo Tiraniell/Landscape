c85382e3b244b1785992df429bf0bf956684626c
Roma Ag <93056140+Tiraniell@users.noreply.github.com>
2022-04-25 21:31:52 +0300

    Lighting
    Lighting

--- Assets/Editor/Window.cs
+++ Assets/Editor/Window.cs
@@ -1,84 +1,84 @@
-using UnityEditor;
-using UnityEngine;
+//using UnityEditor;
+//using UnityEngine;
 
-public class Window : EditorWindow
-{
+//public class Window : EditorWindow
+//{
     
-    public Color myColor;         // Градиент цвета
-    public MeshRenderer GO;      // Ссылка на рендер объекта
+//    public Color myColor;         // Градиент цвета
+//    public MeshRenderer GO;      // Ссылка на рендер объекта
 
-    public Material newMat;
-    private Transform MainCam;
+//    public Material newMat;
+//    private Transform MainCam;
 
-    [MenuItem("Инструменты /Окна/ Генератор префабов")]
+//    [MenuItem("Инструменты /Окна/ Генератор префабов")]
 
-    public static void ShowMyWindow()
-    {
-        GetWindow(typeof(Window), false, "Генератор префабов");
-    }
+//    public static void ShowMyWindow()
+//    {
+//        GetWindow(typeof(Window), false, "Генератор префабов");
+//    }
 
-    void OnGUI()
-    {
-        GO = EditorGUILayout.ObjectField("Меш обьекта", GO, typeof(MeshRenderer), true) as MeshRenderer;
-        newMat = EditorGUILayout.ObjectField("Материал обьекта", GO, typeof(Material), true) as Material;
+//    void OnGUI()
+//    {
+//        GO = EditorGUILayout.ObjectField("Меш обьекта", GO, typeof(MeshRenderer), true) as MeshRenderer;
+//        newMat = EditorGUILayout.ObjectField("Материал обьекта", GO, typeof(Material), true) as Material;
 
-        if (GO)
-        {
-            myColor = RGBSlider(new Rect(10, 30, 200, 20), myColor);  // Отрисовка пользовательского набора слайдеров для получения градиента цвета
-            GO.sharedMaterial.color = myColor; // Покраска объекта
-        }
-        else
-        {
-            if (GUI.Button(new Rect(10, 60, 100, 30), "Создать"))
-            {
-                MainCam = Camera.main.transform;
+//        if (GO)
+//        {
+//            myColor = RGBSlider(new Rect(10, 30, 200, 20), myColor);  // Отрисовка пользовательского набора слайдеров для получения градиента цвета
+//            GO.sharedMaterial.color = myColor; // Покраска объекта
+//        }
+//        else
+//        {
+//            if (GUI.Button(new Rect(10, 60, 100, 30), "Создать"))
+//            {
+//                MainCam = Camera.main.transform;
 
-                GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                MeshRenderer GORender = temp.GetComponent<MeshRenderer>();
-                GORender.sharedMaterial = newMat;
-                temp.transform.position = new Vector3(MainCam.position.x, MainCam.position.y, MainCam.position.z - 10f);
-                GO = GORender;
-            }
-        }
+//                GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+//                MeshRenderer GORender = temp.GetComponent<MeshRenderer>();
+//                GORender.sharedMaterial = newMat;
+//                temp.transform.position = new Vector3(MainCam.position.x, MainCam.position.y, MainCam.position.z - 10f);
+//                GO = GORender;
+//            }
+//        }
 
-        if (GUI.Button(new Rect(10, 160, 100, 30), "Удалить"))
-        {
-            DestroyImmediate(GO.gameObject);
-            GO = null;
+//        if (GUI.Button(new Rect(10, 160, 100, 30), "Удалить"))
+//        {
+//            DestroyImmediate(GO.gameObject);
+//            GO = null;
 
-        }
-    }
+//        }
+//    }
 
-    // Отрисовка пользовательского слайдера
-    float LabelSlider(Rect screenRect, float sliderValue, float sliderMaxValue, string labelText) // ДЗ добавить MinValue
-    {
-        // создаём прямоугольник с координатами в пространстве и заданой шириной и высотой 
-        Rect labelRect = new Rect(screenRect.x, screenRect.y, screenRect.width / 2, screenRect.height);
+//    // Отрисовка пользовательского слайдера
+//    float LabelSlider(Rect screenRect, float sliderValue, float sliderMaxValue, string labelText) // ДЗ добавить MinValue
+//    {
+//        // создаём прямоугольник с координатами в пространстве и заданой шириной и высотой 
+//        Rect labelRect = new Rect(screenRect.x, screenRect.y, screenRect.width / 2, screenRect.height);
 
-        GUI.Label(labelRect, labelText);   // создаём Label на экране
+//        GUI.Label(labelRect, labelText);   // создаём Label на экране
 
-        Rect sliderRect = new Rect(screenRect.x + screenRect.width / 2, screenRect.y, screenRect.width / 2, screenRect.height); // Задаём размеры слайдера
-        sliderValue = GUI.HorizontalSlider(sliderRect, sliderValue, 0.0f, sliderMaxValue); // Вырисовываем слайдер и считываем его параметр
-        return sliderValue; // Возвращаем значение слайдера
-    }
+//        Rect sliderRect = new Rect(screenRect.x + screenRect.width / 2, screenRect.y, screenRect.width / 2, screenRect.height); // Задаём размеры слайдера
+//        sliderValue = GUI.HorizontalSlider(sliderRect, sliderValue, 0.0f, sliderMaxValue); // Вырисовываем слайдер и считываем его параметр
+//        return sliderValue; // Возвращаем значение слайдера
+//    }
 
-    // Отрисовка тройной слайдер группы, каждый слайдер отвечает за свой цвет
-    Color RGBSlider(Rect screenRect, Color rgb)
-    {
-        // Используя пользовательский слайдер, создаём его
-        rgb.r = LabelSlider(screenRect, rgb.r, 1.0f, "Red");
+//    // Отрисовка тройной слайдер группы, каждый слайдер отвечает за свой цвет
+//    Color RGBSlider(Rect screenRect, Color rgb)
+//    {
+//        // Используя пользовательский слайдер, создаём его
+//        rgb.r = LabelSlider(screenRect, rgb.r, 1.0f, "Red");
 
-        // делаем промежуток
-        screenRect.y += 20;
-        rgb.g = LabelSlider(screenRect, rgb.g, 1.0f, "Green");
+//        // делаем промежуток
+//        screenRect.y += 20;
+//        rgb.g = LabelSlider(screenRect, rgb.g, 1.0f, "Green");
 
-        screenRect.y += 20;
-        rgb.b = LabelSlider(screenRect, rgb.b, 1.0f, "Blue");
+//        screenRect.y += 20;
+//        rgb.b = LabelSlider(screenRect, rgb.b, 1.0f, "Blue");
 
-        screenRect.y += 20;
-        rgb.a = LabelSlider(screenRect, rgb.a, 1.0f, "alpha");
+//        screenRect.y += 20;
+//        rgb.a = LabelSlider(screenRect, rgb.a, 1.0f, "alpha");
 
-        return rgb; // возвращаем цвет
-    }
-}
+//        return rgb; // возвращаем цвет
+//    }
+//}
 
